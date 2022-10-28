8771aad8a5e5e93c1a5d529b40cfb41c47ec106e
vighnu2255 <77626351+vighnu2255@users.noreply.github.com>
2022-10-28 12:15:35 -0400

    Added getPosition and updated hire method

--- java/com/aa/act/interview/org/Organization.java
+++ java/com/aa/act/interview/org/Organization.java
@@ -1,10 +1,16 @@
 package com.aa.act.interview.org;
 
 import java.util.Optional;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
 
 public abstract class Organization {
 
 	private Position root;
+	private static int empIdentifier;
+	
 	
 	public Organization() {
 		root = createOrganization();
@@ -20,8 +26,30 @@ public abstract class Organization {
 	 * @return the newly filled position or empty if no position has that title
 	 */
 	public Optional<Position> hire(Name person, String title) {
-		//your code here
-		return Optional.empty();
+		Position position = getPosition(title);
+		if(position != null){
+			position.setEmployee(Optional.of(new Employee(++empIdentifier, person)));
+		}
+		return Optional.ofNullable(position);
+	}
+	
+	private Position getPosition(String title){
+		Set<Position> set = new HashSet<>();
+		Queue<Position> queue = new LinkedList<>();
+		queue.add(root);
+		while(!queue.isEmpty()){
+			Position pos = queue.poll();
+			if(pos.getTitle().equals(title)){
+				return pos;
+			}
+			for(Position position: pos.getDirectReports()){
+				if(!set.contains(position)){
+					set.add(position);
+				}
+			}
+			set.add(pos);
+		}
+		return null;
 	}
 
 	@Override
